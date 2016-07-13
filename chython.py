03813133b12c064495de3f20e9a9b28bb0f9f711
james727 <jameshkatz@gmail.com>
2016-07-13 16:09:27 -0400

    updated pawn logic

--- chython.py
+++ chython.py
@@ -2,13 +2,13 @@ class chess_game:
     def __init__(self):
         self.pieces = {}
         self.board = [['00','00','00','00','00','00','00','00','00','00'],
-                    ['00','WR','WN','WB','WQ','WK','WB','WN','WR','00'],
-                    ['00','WP','WP','WP','WP','WP','WP','WP','WP','00'],
+                    ['00','WR','WN','WB','WQ','WK','--','WN','WR','00'],
+                    ['00','WP','WP','WP','--','WP','BP','WP','WP','00'],
+                    ['00','--','--','--','--','--','--','--','--','00'],
+                    ['00','--','--','--','WP','BP','--','--','--','00'],
                     ['00','--','--','--','--','--','--','--','--','00'],
                     ['00','--','--','--','--','--','--','--','--','00'],
-                    ['00','--','--','--','--','--','--','--','--','00'],
-                    ['00','--','--','--','--','--','--','--','--','00'],
-                    ['00','BP','BP','BP','BP','BP','BP','BP','BP','00'],
+                    ['00','BP','BP','BP','BP','--','BP','BP','BP','00'],
                     ['00','BR','BN','BB','BQ','BK','BB','BN','BR','00'],
                     ['00','00','00','00','00','00','00','00','00','00']]
         self.side_to_move = 'black'
@@ -33,42 +33,86 @@ class chess_game:
             return square
         return False
 
+    def get_promotion_moves(self, starting_location, ending_location):
+        promotion_pieces = ['N', 'B', 'R', 'Q']
+        moves = []
+        for piece in promotion_pieces:
+             moves.append(chess_move('P', starting_location, ending_location, piece))
+        return moves
+
     def get_possible_pawn_moves(self, location):
         chess_moves = []
         # check 1 move up
         if self.side_to_move == 'white':
             front = str(int(location[0])+1)+location[1]
-        else:
+            if not self.check_occupancy(front):
+                if front[0] == '8':
+                    promotions = self.get_promotion_moves(location, front)
+                    chess_moves += promotions
+                else:
+                    chess_moves.append(chess_move('P', location, front))
+
+        if self.side_to_move == 'black':
             front = str(int(location[0])-1)+location[1]
-        if not self.check_occupancy(front):
-            chess_moves.append(chess_move('P', location, front))
+            if not self.check_occupancy(front):
+                if front[0] == '1':
+                    promotions = self.get_promotion_moves(location, front)
+                    chess_moves += promotions
+                else:
+                    chess_moves.append(chess_move('P', location, front))
 
         # check 2 moves up
         if self.side_to_move == 'white' and location[0]=='2':
             frontfront = str(int(location[0])+2)+location[1]
             if not self.check_occupancy(frontfront):
                 chess_moves.append(chess_move('P', location, frontfront))
+
         elif self.side_to_move == 'black' and location[0]=='7':
             frontfront = str(int(location[0])-2)+location[1]
             if not self.check_occupancy(frontfront):
                 chess_moves.append(chess_move('P', location, frontfront))
 
+        #check attacking moves
         if self.side_to_move == 'white':
             left_diag = (int(location[0])+1, int(location[1])-1)
             right_diag = (int(location[0])+1, int(location[1])+1)
             diagonals = [left_diag, right_diag]
             for diag in diagonals:
                 if self.board[diag[0]][diag[1]][0] == 'B':
-                    chess_moves.append(chess_move('P', location, str(diag[0])+str(diag[1])))
+                    if location[0] == '7':
+                        promotions = self.get_promotion_moves(location, str(diag[0])+str(diag[1]))
+                        chess_moves += promotions
+                    else:
+                        chess_moves.append(chess_move('P', location, str(diag[0])+str(diag[1])))
         if self.side_to_move == 'black':
             left_diag = (int(location[0])-1, int(location[1])-1)
             right_diag = (int(location[0])-1, int(location[1])+1)
             diagonals = [left_diag, right_diag]
             for diag in diagonals:
                 if self.board[diag[0]][diag[1]][0] == 'W':
-                    chess_moves.append(chess_move('P', location, str(diag[0])+str(diag[1])))
+                    if location[0] == '2':
+                        promotions = self.get_promotion_moves(location, str(diag[0])+str(diag[1]))
+                        chess_moves += promotions
+                    else:
+                        chess_moves.append(chess_move('P', location, str(diag[0])+str(diag[1])))
 
-
+        #en passant
+        if self.side_to_move == 'white':
+            if location[0] == '5':
+                if self.previous_move != None and self.previous_move.piece_type == 'P':
+                    if self.previous_move.starting_location[0]=='7' and self.previous_move.ending_location[0] == '5':
+                        if self.previous_move.ending_location[1]==str(int(location[1])-1) or self.previous_move.ending_location[1]==str(int(location[1])+1):
+                            ending_column = self.previous_move.ending_location[1]
+                            move = chess_move('P', location, str(int(location[0])+1)+ending_column)
+                            chess_moves.append(move)
+        if self.side_to_move == 'black':
+            if location[0] == '4':
+                if self.previous_move != None and self.previous_move.piece_type == 'P':
+                    if self.previous_move.starting_location[0]=='2' and self.previous_move.ending_location[0] == '4':
+                        if self.previous_move.ending_location[1]==str(int(location[1])-1) or self.previous_move.ending_location[1]==str(int(location[1])+1):
+                            ending_column = self.previous_move.ending_location[1]
+                            move = chess_move('P', location, str(int(location[0])-1)+ending_column)
+                            chess_moves.append(move)
 
 
         return chess_moves
@@ -120,17 +164,23 @@ class chess_game:
 
 
 class chess_move:
-    def __init__(self, piece_type, starting_location, ending_location, promotion = False):
+    def __init__(self, piece_type, starting_location, ending_location, promotion = False, capture = False):
         self.piece_type = piece_type
         self.starting_location = starting_location
         self.ending_location = ending_location
+        self.promotion = promotion
+        self.capture = capture
 
     def print_move(self):
-        print self.piece_type+","+self.starting_location+","+self.ending_location
+        if self.promotion:
+            print self.piece_type+","+self.starting_location+","+self.ending_location+","+self.promotion
+        else:
+            print self.piece_type+","+self.starting_location+","+self.ending_location
 
 
 c = chess_game()
-#moves = c.get_all_legal_moves()
-#for move in moves:
-#    move.print_move()
 c.print_board()
+#c.previous_move = chess_move('P', '24','44')
+moves = c.get_all_legal_moves()
+for move in moves:
+    move.print_move()
