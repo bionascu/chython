7e3c63c8ef53aa5fada40cf658541169b263dd53
james727 <jameshkatz@gmail.com>
2016-07-14 18:44:37 -0400

    small updates to gui

--- gui_chython.py
+++ gui_chython.py
@@ -15,6 +15,25 @@ class chess_gui:
         for i in range(len(moves)):
             move = moves[i]
             print "  "+str(i+1)+'. '+move.print_move()
+        player_move = raw_input('\nChoose your move from the above list. Type only the number for the move you desire.\n\n')
+        if self.validate_move(player_move, moves):
+            self.execute_move(moves[int(player_move)-1])
+        else:
+            print "Wrong"
+
+    def validate_move(self,player_input,moves):
+        move_list = range(1,len(moves)+1)
+        if int(player_input) not in move_list:
+            return False
+        return True
+
+    def execute_move(self, move):
+        print "\nThank you for the move! Executing your move now. The updated board is below:\n"
+        self.game.update_board(move)
+        self.game.print_board()
+        self.prompt_move()
+
+
 
 
 
