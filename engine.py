9be338aa73059d8bdaf67483337b09a46c7ade61
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2016-08-01 10:15:58 -0400

    knight, bishop & rook position evaluation functions

--- engine.py
+++ engine.py
@@ -8,7 +8,7 @@ KING_SAFETY_WEIGHT = 1
 KING_CENTRALITY_WEIGHT = 1
 
 def evaluate_material_score(chess_game):
-    side_to_move = chess_game.side_to_move[0].upper()
+    side_to_move = chess_game.side_to_move
     side_to_move_counter = 0
     other_side_counter = 0
     piece_values = {'Q':9,'R':5,'B':3,'N':3,'P':1,'K':500}
@@ -21,7 +21,31 @@ def evaluate_material_score(chess_game):
     return side_to_move_counter-other_side_counter
 
 def evaluate_knight_positions(chess_game): # B
-    pass
+    side_to_move = chess_game.side_to_move 
+    side_to_move_counter = 0
+    other_side_counter = 0
+    max_value = 8.0
+    values = [1,2,3,4]
+    position_values = [[0]*9]*9
+    for i in range(1, 9):
+        for j in range(1, 9):
+            if (i in [4,5]) and (j in range(4, 6)):
+                position_values[i][j] = values[3]
+            if i in (3,6) and j in range(3, 7):
+                position_values[i][j] = values[2]
+            if i in (2,7) and j in range(2, 8):
+                position_values[i][j] = values[1]
+            if i in (1,8):
+                position_values[i][j] = values[0]
+
+            if chess_game.board[i][j][1] == 'N':
+                if chess_game.board[i][j][0] == side_to_move:
+                    side_to_move_counter += position_values[i][j]
+                else:
+                    other_side_counter += position_values[i][j]
+    # Scaling
+    scaled_value = (side_to_move_counter - other_side_counter) / max_value
+    return scaled_value
 
 def get_doubled_pawns(chess_game, pawn_color):
     score = 2
@@ -62,10 +86,64 @@ def evaluate_pawn_structure(chess_game): # J
 
 
 def evaluate_bishop_positions(chess_game): # B
-    pass
+    side_to_move = chess_game.side_to_move
+    side_to_move_counter = 0
+    opponent_counter = 0
+    bonus = 6.0
+    max_value = 14.0 + bonus
+    # Possible attacking squares
+    side_bishop_moves = [move for move in chess_game.get_all_legal_moves() if move.piece_type == 'B']
+    side_to_move_counter += len(side_bishop_moves)
+    chess_game.side_to_move = chess_game.opponent_color
+    opponent_bishop_moves = [move for move in chess_game.get_all_legal_moves() if move.piece_type == 'B']
+    opponent_counter += len(opponent_bishop_moves)
+    chess_game.side_to_move = side_to_move
+    # Bonus for both bishops
+    bishop_count = {'side':0, 'opponent':0}
+    for row in chess_game.board:
+        for occupant in row:
+            if occupant[1] == 'B':
+                if occupant[0] == side_to_move:
+                    bishop_count['side'] += 1
+                else:
+                    bishop_count['opponent'] += 1
+    if bishop_count['side'] == 2: side_to_move_counter += bonus
+    if bishop_count['opponent'] == 2: opponent_counter += bonus
+    # Scaling
+    scaled_value = (side_to_move_counter - opponent_counter) / max_value
+    return scaled_value
 
 def evaluate_rook_positions(chess_game): # B
-    pass
+    side_to_move = chess_game.side_to_move
+    side_to_move_counter = 0
+    opponent_counter = 0
+    bonus = 2.0
+    max_value = 28.0 + bonus
+    # Possible attacking squares
+    side_rook_moves = [move for move in chess_game.get_all_legal_moves() if move.piece_type == 'R']
+    side_to_move_counter += len(side_rook_moves)
+    chess_game.side_to_move = chess_game.opponent_color
+    opponent_rook_moves = [move for move in chess_game.get_all_legal_moves() if move.piece_type == 'R']
+    opponent_counter += len(opponent_rook_moves)
+    chess_game.side_to_move = side_to_move
+    # Bonus for 7th rank 
+    for occupant in chess_game.board[7]:
+        if occupant[1] == 'R':
+            if occupant[0] == 'W':
+                if side_to_move == 'W':
+                    side_to_move_counter += bonus
+                else:
+                    opponent_counter += bonus
+    for occupant in chess_game.board[2]:
+        if occupant[1] == 'R':
+            if occupant[0] == 'B':
+                if side_to_move == 'B':
+                    side_to_move_counter += bonus
+                else:
+                    opponent_counter += bonus
+    # Scaling
+    scaled_value = (side_to_move_counter - opponent_counter) / max_value
+    return scaled_value
 
 def get_passed_pawn_score_in_this_column(chess_game, column, color):
     passed = 0
